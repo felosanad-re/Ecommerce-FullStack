5f503780cfe833d894c56e1f97588290572eaa1a
felosanad <felosanad@gmail.com>
2026-05-11 14:14:12 +0300

    refactor in application

--- Solution.Talabat/Felo.Talabat.first/obj/Debug/net9.0/Felo.Talabat.Api.AssemblyInfo.cs
+++ Solution.Talabat/Felo.Talabat.first/obj/Debug/net9.0/Felo.Talabat.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Felo.Talabat.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30ec6250ca5b76aaeadebdf7fa46c2fa5f0748d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5cd715a4a003170c0ff8a50669f72fc170eff2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Felo.Talabat.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Felo.Talabat.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
